78d21ef4cfdca9bf0a7886f8e62a7fb861124c97
joem3847 <joem3847@gmail.com>
2024-02-27 10:54:50 -0600

    Error handling

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -4,7 +4,7 @@ import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
   title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  description: z.string().min(1)
 });
 
 export async function POST(request: NextRequest) {
@@ -15,7 +15,7 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(validation.error.errors, { status: 400 });
 
   const newIssue = await prisma.issue.create({
-    data: { title: body.title, description: body.description },
+    data: { title: body.title, description: body.description }
   });
 
   return NextResponse.json(newIssue, { status: 201 });
