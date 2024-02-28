7372880ae36ee9c7bb287449d5e21baa0887fefc
joem3847 <joem3847@gmail.com>
2024-02-28 17:26:07 -0600

    Add delete button

--- app/issues/IssueActions.tsx
+++ app/issues/IssueActions.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
+import { PlusIcon } from "@radix-ui/react-icons";
 
 const IssueActions = () => {
   return (
     <div className='mb-5'>
       <Button>
+        <PlusIcon />
         <Link href='/issues/new'>New Issue</Link>
       </Button>
     </div>
